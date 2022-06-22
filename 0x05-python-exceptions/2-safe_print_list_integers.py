da4cbef40de9f46986b983d9ef2cc2379aadb832
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-06-23 01:01:32 +0300

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    ret = 0
-    for i in range(0, x):
+    num = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
+            print("{:d}".format(my_list[i]), end='')
+            num += 1
         except (ValueError, TypeError):
-            continue
-    print("")
-    return (ret)
-© 2022 GitHub, Inc.
-Terms
+            x += 1
+    print('')
+    return num
