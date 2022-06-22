9a526648d4f280aad17304d0d44799990d9be8e4
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-06-23 00:57:48 +0300

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    nb_print = 0
+    num = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            nb_print += 1
+            print(my_list[i], end='')
+            num += 1
         except IndexError:
             break
-    print("")
-    return (nb_print)
-© 2022 GitHub, Inc.
-Terms
+    print('')
+    return num
